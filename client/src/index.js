dc6f13fb56fd414baca6c898f2fe9c96445d28ba
Stefano Consonni <stefanoconsonni@ymail.com>
2023-04-06 20:11:06 +0200

    setup redux state

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import "./index.css";
+import { api } from "state/api";
+// Redux
+import { configureStore } from "@reduxjs/toolkit";
+import globalReducer from "state";
+import { Provider } from "react-redux";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
+});
+setupListeners(store.dispatch);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
