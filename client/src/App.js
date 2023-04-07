10012c3a67236012b414e7b939b81f4a378ef1b8
Stefano Consonni <stefanoconsonni@ymail.com>
2023-04-07 19:36:18 +0200

    create Dashboard component

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ import Performance from "scenes/performance";
 function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
       <BrowserRouter>
