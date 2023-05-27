2494506606622b7d13cd818e25f14ecc610be2c9
Stefano Consonni <stefanoconsonni@ymail.com>
2023-05-27 15:20:52 +0200

    edit setMode import in Navbar.jsx

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { setMode } from "state";
+import { setMode } from "state/globalSlice";
 import {
   LightModeOutlined,
   DarkModeOutlined,
