5e36077d031e033dfc4a0925a92b811ec0d5afa8
Stefano Consonni <stefanoconsonni@ymail.com>
2023-05-28 10:58:39 +0200

    add Navbar component

--- client/src/scenes/layout/Layout.jsx
+++ client/src/scenes/layout/Layout.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
+import Navbar from "components/Navbar";
 import Sidebar from "components/Sidebar";
 import { useGetUserQuery } from "state/api";
 
