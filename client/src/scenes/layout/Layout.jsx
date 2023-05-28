eb154d197f8417e1f915e39b1299b25cc215ab0f
Stefano Consonni <stefanoconsonni@ymail.com>
2023-05-28 10:54:31 +0200

    delete Navbar component

--- client/src/scenes/layout/Layout.jsx
+++ client/src/scenes/layout/Layout.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
-import Navbar from "components/Navbar";
 import Sidebar from "components/Sidebar";
 import { useGetUserQuery } from "state/api";
 
