9396025eab368287df1ddd999306b5192c0f0078
Stefano Consonni <stefanoconsonni@ymail.com>
2023-06-03 23:51:03 +0200

    add useEffect for responsive in Navabar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { setMode } from "state/globalSlice";
 import {
@@ -19,6 +19,7 @@ import {
   Menu,
   MenuItem,
   useTheme,
+  useMediaQuery,
 } from "@mui/material";
 import FlexBetween from "components/FlexBetween";
 import profileImage from "assets/profile.jpeg";
@@ -26,11 +27,18 @@ import profileImage from "assets/profile.jpeg";
 const Navbar = ({ user, isSidebarOpen, setIsSidebarOpen }) => {
   const dispatch = useDispatch();
   const theme = useTheme();
+  const isLargeScreen = useMediaQuery("(min-width: 1000px)");
 
   const [anchorEl, setAnchorEl] = useState(null);
   const isOpen = Boolean(anchorEl);
   const handleClick = (event) => setAnchorEl(event.currentTarget);
   const handleClose = () => setAnchorEl(null);
+
+  useEffect(() => {
+    if (!isLargeScreen && isSidebarOpen) {
+      setIsSidebarOpen(!isSidebarOpen);
+    }
+  }, [isLargeScreen, isSidebarOpen, setIsSidebarOpen]);
 
   return (
     <AppBar
