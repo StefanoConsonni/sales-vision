8f870f888cf56003924d23fa9da8d23d46dcf1d8
Stefano Consonni <stefanoconsonni@ymail.com>
2023-06-11 21:53:09 +0200

    edit dataUser mock data

--- server/data/index.js
+++ server/data/index.js
@@ -18052,13 +18052,13 @@ export const dataUser = [
   },
   {
     _id: "63701cc1f03239b7f700000e",
-    name: "Shelly",
+    name: "Rachel",
     email: "swelbeck12@ycombinator.com",
     password: "RSjzmAjnq",
     city: "Sangoleng",
     state: null,
     country: "ID",
-    occupation: "Pharmacist",
+    occupation: "Sales manager",
     phoneNumber: "7036619983",
     transactions: [
       "63701d74f03239d81e000027",
